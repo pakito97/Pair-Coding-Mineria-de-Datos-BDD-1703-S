505e23c36a272efc69b76aae56845b15fd18b3fc
Marco Antonio Rodriguez Medrano <45216573+pakito97@users.noreply.github.com>
2021-03-15 17:58:50 -0700

    inserción de comentarios
    se le agregaron los comentarios al código

--- Unit_1/Practices/Practice_1.R
+++ Unit_1/Practices/Practice_1.R
@@ -1,19 +1,28 @@
-# Size
+#Test the Law Of Large Numbers for N random normally distributed numbers with mean = 0, stdev=1:
+
+#Create an R script that will count how many of these numbers fall between -1 and 1 and divide
+#by the total quantity of N
+
+#You know that E(X) = 68.2%
+#Check that Mean(Xn)->E(X) as you rerun your script while increasing N
+
+
+#1 Initialize sample size(Size)
 N <- 1000000
 
-# Counter
+#2 Initialize counter(Counter)
 counter <- 0
 
-# Loop
+#3 loop for(i in rnorm(size))(Loop)
 for (i in rnorm(N)) {
-  # Numbers fall between -1 and 1
+  #4 Check if the iterated variable falls(Numbers fall between -1 and 1)
   if (i >= -1 & i <= 1) {
-    # Counter increment
+    #5 Increase counter if the condition is true(Counter increment)
     counter <- counter + 1
   }
 }
 
-# Return
+#6 return a result <- counter / N(Return)
 result = counter / N
 
 # Log
